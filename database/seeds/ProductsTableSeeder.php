74b65faa0c77745f2a5e27fcd6972a9060f5d81f
quangdvn <ghitacluber@gmail.com>
2019-11-15 17:43:45 +0700

    Change seed file for using Voyager

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -19,6 +19,7 @@ class ProductsTableSeeder extends Seeder
                 'slug' => 'laptop-' . $i,
                 'details' => [13, 14, 15][array_rand([13, 14, 15])] . ' inch, ' . [1, 2, 3][array_rand([1, 2, 3])] . ' TB SSD, 32GB RAM',
                 'price' => rand(149999, 249999),
+                'image' => 'products\November2019\laptop-' . $i . '.jpg',
                 'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
             ])->categories()->attach(1);
         }
@@ -34,6 +35,7 @@ class ProductsTableSeeder extends Seeder
                 'slug' => 'desktop-' . $i,
                 'details' => [24, 25, 27][array_rand([24, 25, 27])] . ' inch, ' . [1, 2, 3][array_rand([1, 2, 3])] . ' TB SSD, 32GB RAM',
                 'price' => rand(249999, 449999),
+                'image' => 'products\November2019\desktop-' . $i . '.jpg',
                 'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
             ])->categories()->attach(2);
         }
@@ -45,6 +47,7 @@ class ProductsTableSeeder extends Seeder
                 'slug' => 'phone-' . $i,
                 'details' => [16, 32, 64][array_rand([16, 32, 64])] . 'GB, 5.' . [7, 8, 9][array_rand([7, 8, 9])] . ' inch screen, 4GHz Quad Core',
                 'price' => rand(79999, 149999),
+                'image' => 'products\November2019\phone-' . $i . '.jpg',
                 'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
             ])->categories()->attach(3);
         }
@@ -56,6 +59,7 @@ class ProductsTableSeeder extends Seeder
                 'slug' => 'tablet-' . $i,
                 'details' => [16, 32, 64][array_rand([16, 32, 64])] . 'GB, 5.' . [10, 11, 12][array_rand([10, 11, 12])] . ' inch screen, 4GHz Quad Core',
                 'price' => rand(49999, 149999),
+                'image' => 'products\November2019\tablet-' . $i . '.jpg',
                 'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
             ])->categories()->attach(4);
         }
@@ -67,6 +71,7 @@ class ProductsTableSeeder extends Seeder
                 'slug' => 'tv-' . $i,
                 'details' => [46, 50, 60][array_rand([7, 8, 9])] . ' inch screen, Smart TV, 4K',
                 'price' => rand(79999, 149999),
+                'image' => 'products\November2019\tv-' . $i . '.jpg',
                 'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
             ])->categories()->attach(5);
         }
@@ -78,6 +83,7 @@ class ProductsTableSeeder extends Seeder
                 'slug' => 'camera-' . $i,
                 'details' => 'Full Frame DSLR, with 18-55mm kit lens.',
                 'price' => rand(79999, 249999),
+                'image' => 'products\November2019\camera-' . $i . '.jpg',
                 'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
             ])->categories()->attach(6);
         }
@@ -89,6 +95,7 @@ class ProductsTableSeeder extends Seeder
                 'slug' => 'appliance-' . $i,
                 'details' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolorum!',
                 'price' => rand(79999, 149999),
+                'image' => 'products\November2019\appliance-' . $i . '.jpg',
                 'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
             ])->categories()->attach(7);
         }
@@ -96,6 +103,5 @@ class ProductsTableSeeder extends Seeder
         //* Select random products to set as featured
         Product::whereIn('id', [1, 12, 22, 31, 41, 43, 47, 51, 53, 61, 69, 73, 80])
                 ->update(['featured' => true]);
-
     }
 }
